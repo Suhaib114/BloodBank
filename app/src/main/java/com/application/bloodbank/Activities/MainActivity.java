823b9a853c4e512bbf38a6437f463bbad2a61892
DESKTOP-T2P8DRQ\Suhaib <suhaibahmed114@gmail.com>
2021-12-20 14:50:26 +0600

    First Commit

---
+++ app/src/main/java/com/application/bloodbank/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.application.bloodbank.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.application.bloodbank.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/application/bloodbank/Activities/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.application.bloodbank.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.application.bloodbank.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 2500);
+    }
+}
